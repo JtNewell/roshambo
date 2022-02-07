32efbcbc9214b180ab5c794fde0e4cf1c113f5cc
jtnew <jtnewellbusiness@gmail.com>
2022-02-06 22:48:42 -0500

    Add README, basic html, and js rough outline

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # roshambo
+
+Here's my first attempt at The Odin Project's Rock, Paper, Scissors project. 
---
+++ index.js
@@ -0,0 +1,51 @@
+// Define Variables
+// -computerInput
+// -userInput
+
+
+
+/*What conditions exist in this game?
+    - All the possible combinations
+
+            *WIN*           
+     -Rock > Scissors
+     -Paper > Rock
+     -Scissors > Paper
+
+            *LOSE*
+     -Rock < Paper
+     -Paper < Scissors
+     -Scissors < Rock
+
+            *TIE*
+     -Rock = Rock
+     -Paper = Paper
+     -Scissors = Scissors
+
+    *Put in Rock, Paper, or Scissors*
+     -No Input / Not 1 of 3 variables
+
+             *console.log()*
+     If tie say "You tied! Try again?"
+     If lose say "You Lose! `computerInput` beats `userInput`!"
+     If win say "You Win! `userIput` beats `computerInput`!"
+
+            *Keep in mind*
+     Make userInput case insensitive
+
+            *Functions*
+    -computerPlay: randomly returns either "Rock", "Paper" or "Scissors"
+
+    -playRound: checks userInput and computerInput and returns a string
+    that declares the winner of the round. (see reponses above)
+
+    -game(): Use previous function inside of this one to play a 5 round game that
+    keeps score and reports a winner of loser at the end. 
+
+*/
+
+
+
+function computerPlay() {
+
+}
